98dfc0784fdc0b99f52dd77b590ec0132e18ed25
erlendsamstad <43167143+erlendsamstad@users.noreply.github.com>
2020-03-13 14:19:54 +0100

    Add files via upload

---
+++ Chemical.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using UnityEngine;
+
+/************************************
+ * File name:       Chemical.cs
+ * Creation date:   12/09/2019
+ * Author:          Erlend Samstad
+ * 
+ * Description:
+ * Contains all data necessary for a
+ * chemical.
+ * **********************************/
+
+namespace BigBrainIndie.XtremeLabSafety.Classes {
+    [Serializable]
+    public class Chemical {
+
+        [Serializable]
+        public enum Traits {
+            na,
+            caustic,
+            flammable,
+            flashbang,
+            foaming,
+            foggy,
+            toxic,
+            explosive,
+            water,
+            inert
+        }
+
+        // Chemical data
+        public string chemicalName;
+        public Color color;
+        public float amount;
+        public Traits trait;
+        public bool compound;
+
+        // Getters
+        public string ChemicalName => chemicalName;
+        public Color Color => color;
+        public float Amount => amount;
+        public Traits Trait => trait;
+        public bool Compound => compound;
+
+
+        // Sets a standard undefined chemical when no data is set
+        public Chemical () {
+            this.chemicalName = "<Unnamed>";
+            this.color = ChemicalColor.NA;
+            this.amount = 0;
+            this.trait = Traits.na;
+        }
+
+        public Chemical (string _name, Color _color) {
+            this.chemicalName = _name;
+            this.color = _color;
+            this.amount = 100;
+            this.trait = Traits.na;
+        }
+
+        // Sets the name and color of the chemical
+        public Chemical (string _name, Color _color, Traits _trait) {
+            this.chemicalName = _name;
+            this.color = _color;
+            this.amount = 100;
+            this.trait = _trait;
+        }
+
+        public void ChangeName (string _name) {
+            this.chemicalName = _name;
+        }
+
+        public void ChangeColor (Color _color) {
+            this.color = _color;
+        }
+
+        public void SetAmount (float _amount) {
+            this.amount = _amount;
+            this.amount = Mathf.Clamp(this.amount, 0, 100);
+        }
+
+        public void AddAmount (float _amount) {
+            this.amount += _amount;
+            this.amount = Mathf.Clamp(this.amount, 0, 100);
+        }
+
+        public void ChangeTrait (Traits _trait) {
+            this.trait = _trait;
+        }
+
+        public void IsCompound (bool _c) {
+            this.compound = _c;
+        }
+    }
+
+    public struct ChemicalColor {
+
+        public enum ColorSelect {
+            NA,
+            Red,
+            Green,
+            DarkGreen,
+            Blue,
+            DarkBlue,
+            Cyan,
+            DarkCyan,
+            Orange,
+            Yellow,
+            Purple,
+            Magenta,
+            Gray,
+            DarkGray,
+            AquaMarine,
+            Black,
+            Amber,
+            DarkPurple,
+            Brown,
+            DarkYellow,
+            DarkRed,
+            Water
+        }
+
+        // Pre-set of colors
+        public static readonly Color NA = Color.white;
+        public static readonly Color Red = Color.red;
+        public static readonly Color Green = Color.green;
+        public static readonly Color DarkGreen = new Color(0.11f, 0.42f, 0.11f);
+        public static readonly Color Blue = Color.blue;
+        public static readonly Color DarkBlue = new Color(0.043f, 0.325f, 0.58f);
+        public static readonly Color Cyan = Color.cyan;
+        public static readonly Color DarkCyan = new Color(0, 0.663f, 0.439f);
+        public static readonly Color Orange = new Color(1, 0.62f, 0);
+        public static readonly Color Yellow = Color.yellow;
+        public static readonly Color Purple = new Color(0.6f, 0, 1);
+        public static readonly Color Magenta = Color.magenta;
+        public static readonly Color Gray = new Color(0.7f, 0.7f, 0.7f);
+        public static readonly Color DarkGray = new Color(0.369f, 0.369f, 0.369f);
+        public static readonly Color AquaMarine = new Color(0.49f, 1, 0.83f);
+        public static readonly Color Black = Color.black;
+        public static readonly Color Amber = new Color(1, 0.74f, 0);
+        public static readonly Color DarkPurple = new Color(0.314f, 0.043f, 0.686f);
+        public static readonly Color Brown = new Color(0.271f, 0.051f, 0.051f);
+        public static readonly Color DarkYellow = new Color(0.769f, 0.698f, 0.035f);
+        public static readonly Color DarkRed = new Color(0.663f, 0.11f, 0);
+        public static readonly Color Water = new Color(0.5647059f, 0.6841081f, 1);
+    }
+}
---
+++ GameControllerEditor.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEditor;
+using BigBrainIndie.XtremeLabSafety.Controllers;
+using BigBrainIndie.XtremeLabSafety.Profiles;
+using BigBrainIndie.XtremeLabSafety.Classes;
+
+/*******************************
+ * File name:     GameControllerEditor.cs
+ * Creation date: 14/10/2019
+ * Author:        Erlend Samstad
+ * 
+ * Description:
+ * Shows the levels in the
+ * inspector
+ * *****************************/
+
+namespace BigBrainIndie.XtremeLabSafety.Editors {
+    [CustomEditor(typeof(GameController))]
+    public class GameControllerEditor : Editor {
+
+        int showCustomObjectsIndex = 0;
+
+        public override void OnInspectorGUI () {
+            GameController _gc = (GameController)target;
+
+            if (_gc.lep != null) { // lep is the LevelEditorProfile that is place in the GameController
+                foreach (var level in _gc.lep.Levels) {
+                    EditorGUILayout.LabelField("Level ID:", level.ID.ToString());
+                    EditorGUILayout.LabelField("Level Name:", level.LevelName);
+
+                    EditorGUILayout.LabelField("Required chemical:", level.TargetChemical.ToString());
+
+                    EditorGUILayout.LabelField("Chemical Groups:");
+                    for (int i = 0; i < level.ChemicalGroups.Count; i++)
+                        EditorGUILayout.LabelField(" - Chemical Group #" + (i + 1).ToString() + ": " + level.ChemicalGroups[i]);
+
+                    if (level.CustomObjects.Count > 0) {
+                        EditorGUILayout.BeginHorizontal();
+
+                        EditorGUILayout.LabelField("Custom objects:");
+                        if (GUILayout.Button("Show Custom objects"))
+                            showCustomObjectsIndex = _gc.lep.Levels.IndexOf(level);
+
+                        EditorGUILayout.EndHorizontal();
+
+                        foreach (var co in level.CustomObjects) {
+                            if (co == null)
+                                continue;
+
+                            EditorGUILayout.BeginHorizontal();
+
+                            EditorGUILayout.LabelField(" - " + co.name);
+                            if (GUILayout.Button("Reset Position"))
+                                ResetPosition(level, level.CustomObjects.IndexOf(co));
+                            else if (GUILayout.Button("Reset Rotation"))
+                                ResetRotation(level, level.CustomObjects.IndexOf(co));
+
+                            EditorGUILayout.EndHorizontal();
+                        }
+                    }
+                    EditorGUILayout.Space();
+
+                    Rect _lineRect = EditorGUILayout.GetControlRect(false, 3);
+                    _lineRect.height = 3;
+                    EditorGUI.DrawRect(_lineRect, Color.gray);
+
+                    EditorGUILayout.Space();
+                }
+            } else {
+                EditorGUILayout.HelpBox("No Level Editor Profile has been set!", MessageType.Error);
+            }
+
+            DrawDefaultInspector();
+        }
+
+        void OnSceneGUI () {
+            GameController _gc = target as GameController;
+            LevelEditorProfile _l = _gc.lep;
+
+            if (_l != null) {
+                for (int i = 0; i < _l.Levels.Count; i++) {
+                    if (i != showCustomObjectsIndex)
+                        continue;
+
+                    // Displays the rotation and position handle of the custom object
+                    for (int j = 0; j < _l.Levels[i].CustomSpawnPoints.Count; j++) {
+                        _l.Levels[i].customSpawnPoints[j] = Handles.PositionHandle(_l.Levels[i].CustomSpawnPoints[j], _l.Levels[i].CustomRotations[j]);
+                        _l.Levels[i].CustomRotations[j] = Handles.RotationHandle(_l.Levels[i].CustomRotations[j], _l.Levels[i].CustomSpawnPoints[j]);
+
+                        // Adds a label to the object
+                        Handles.Label(_l.Levels[i].CustomSpawnPoints[j] + new Vector3(0.01f, 0.05f), string.Format("Level #{0},\nObject: {1}\n x: {2}, y: {3}, z: {4}",
+                                                                                                                  (i + 1).ToString(),
+                                                                                                                  (_l.Levels[i].CustomObjects[j] != null) ? _l.Levels[i].CustomObjects[j].name : "null",
+                                                                                                                  _l.Levels[i].CustomSpawnPoints[j].x.ToString("0.00"),
+                                                                                                                  _l.Levels[i].CustomSpawnPoints[j].y.ToString("0.00"),
+                                                                                                                  _l.Levels[i].CustomSpawnPoints[j].z.ToString("0.00")));
+
+                    }
+                }
+            }
+        }
+
+        void ResetPosition (Level _level, int i) {
+            _level.customSpawnPoints[i] = Vector3.zero;
+        }
+
+        void ResetRotation (Level _level, int i) {
+            _level.customRotations[i] = Quaternion.identity;
+        }
+    }
+}
